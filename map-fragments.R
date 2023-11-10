b8b38449a47c5ada2676716f35d713b0639181af
KLBoles <boleskellyl@gmail.com>
2023-11-10 07:11:51 -0800

    added horizontal lines

--- map-fragments.R
+++ map-fragments.R
@@ -25,11 +25,13 @@ us_counties18_easternky <- subset(us_counties18sf_ky,COUNTYFP %in% c("195","119"
                                                                      "109","203","199","125",
                                                                      "121"))
 us_counties18_floydco <- subset(us_counties18sf_ky,COUNTYFP %in% c("071"))
+us_states18sf <- subset(us_states18sf, STATEFP %in% c("21"))
 
 
 floyd_co_ky <- ggplot() +
   geom_sf(data=us_counties18_easternky, fill="#ffffff", color="#262626", lwd=.5, show.legend=NA)+
   geom_sf(data=us_counties18_floydco, fill="#0D8C98", color="#262626", lwd=.5, show.legend=NA)+
+  geom_sf(data=us_states18sf, fill=NA, color="#262626", lwd=1, show.legend = NA)+
   theme(text = element_text(family="Roboto Condensed"),
         legend.key.size = unit(1.5,"line"),
         axis.text.x = element_blank(),
